4ef7748962b2b47bb7e5d4d53bd90625b7354812
prasad4264 <115805223+prasad4264@users.noreply.github.com>
2022-10-16 17:11:16 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Fibonacci series of 1 to 50
+n = int(input("Enter your number"))
+n1 = 0
+n2 = 1
+x = 0
+while(x<n):
+    print(n1)
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    x = x + 1
+
+
+# In[ ]:
+
+
+
+
